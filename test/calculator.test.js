da1fe45be12acd0655b0f8dddc35348d817841f7
KiviKong <kevin95ra@gmail.com>
2018-05-12 19:03:38 -0500

    Restructures the tests into different files

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,7 +5,7 @@ chai.should();
 
 describe('Calculator', () => {
     describe('calculateTotalAmount', () => {
-        it(' receive [PANTS, TSHIRT, PANTS, PANTS, HAT, TSHIRT, TSHIRT] should return 74.5', async () => {
+        it('receive [PANTS, TSHIRT, PANTS, PANTS, HAT, TSHIRT, TSHIRT] should return 74.5', async () => {
             let total = calculator.calculateTotalAmount(
                     ['PANTS', 'TSHIRT', 'PANTS', 'PANTS', 'HAT', 'TSHIRT', 'TSHIRT'],
                     [{code: 'PANTS', price: 5},
@@ -16,7 +16,7 @@ describe('Calculator', () => {
             total.should.be.eql(74.5);
         });
 
-        it(' receive [PANTS, TSHIRT, PANTS] should return 25', async () => {
+        it('receive [PANTS, TSHIRT, PANTS] should return 25', async () => {
             let total = calculator.calculateTotalAmount(
                 ['PANTS', 'TSHIRT', 'PANTS'],
                 [{code: 'PANTS', price: 5},
@@ -27,12 +27,12 @@ describe('Calculator', () => {
             total.should.be.eql(25);
         });
 
-        it(' receive [] (empty arrays) should return 0', async () => {
+        it('receive [] (empty arrays) should return 0', async () => {
             let total = calculator.calculateTotalAmount();
             total.should.be.eql(0);
         });
 
-        it(' receive [SCARF, SHOES, SOCKS] should return 0 because this codes are not stored in the db', async () => {
+        it('receive [SCARF, SHOES, SOCKS] should return 0 because this codes are not stored in the db', async () => {
             let total = calculator.calculateTotalAmount(
                 ['SCARF', 'SHOES', 'SOCKS'],
                 [{code: 'PANTS', price: 5},
